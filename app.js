16655dfe03531238b0924ca0368dcc164826367d
azamjonoff <abdullohazamjonov57gmail.com>
2024-04-27 11:27:41 +0500

    add 13 matters

---
+++ app.js
@@ -0,0 +1,135 @@
+// let a = +prompt(`Son kiriting:`);
+// let i = +prompt(`Ishora kiriting:`);
+// let b = +prompt(`Son kiriting:`);
+
+// function calculate(a, b, i) {
+//   switch (i) {
+//     case "+":
+//       console.log(a + b);
+//       break;
+//     case "-":
+//       console.log(a - b);
+//       break;
+//     case "*":
+//       console.log(a * b);
+//       break;
+//     case "/":
+//       console.log(a / b);
+//       break;
+//   }
+// }
+
+// calculate();
+
+// UYGA VAZIFA START
+
+// 1)
+
+// function sum(x, y) {
+//   console.log(x + y);
+// }
+
+// sum(3, 2);
+
+// 2)
+
+// function toSekunds(minutes) {
+//   console.log(`${minutes * 60} sekund`)
+// }
+
+// toSekunds(3)
+
+// 3)
+
+// function nextInteger(int) {
+//   console.log(int + 1);
+// }
+
+// nextInteger(6);
+
+// 4)
+
+// function uchburchakYuzi(asos, balandlik) {
+//   console.log(`Uchburchak yuzi: ${(asos * balandlik) / 2}`);
+// }
+
+// uchburchakYuzi(3, 7);
+
+// 5)
+
+// function ageToDays(age) {
+//   console.log(age * 365);
+// }
+
+// ageToDays(4);
+
+// 6)
+
+// function sonningKubi(num1) {
+//   console.log(num1 ** 3);
+// }
+
+// sonningKubi(2);
+
+// 7)
+
+// function firstElement(arr) {
+//   let result = arr[0]
+// }
+
+// firstElement(1, 2, 7)
+
+// 8)
+
+// function power(x, y) {
+//   console.log(x * y);
+// }
+
+// power(2, 7);
+
+// 9)
+
+// function hourToSecunds(h) {
+//   console.log(h * 3600);
+// }
+
+// hourToSecunds(5);
+
+// 10)
+
+// function uchinchiTomon(tomon1, tomon2) {
+//   console.log(`Uchinchi tomon uzunligi: ${tomon1 + tomon2 - 1}`);
+// }
+
+// uchinchiTomon(3, 9);
+
+// 11)
+
+// function qoldiq(son1, son2) {
+//   console.log(son1 % son2);
+// }
+
+// qoldiq(14, 6);
+
+// 12)
+
+// function TortbYuzi(boyi, eni) {
+//   console.log(boyi * eni);
+// }
+
+// TortbYuzi(3, 4);
+
+// LEKIN NAMUNALARDA PERIMETRINI HISOBLAB QOYIBDI
+
+// 13)
+
+// let text = String(prompt("Text kiriting:"));
+
+// function stringSomething(a) {
+//   a = "something ";
+//   console.log(a + text);
+// }
+
+// stringSomething();
+
+// UYGA VAZIFA END
