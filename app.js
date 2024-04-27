f0120715c2b675d5dd0f5ababe7b9161d30609cf
azamjonoff <abdullohazamjonov57gmail.com>
2024-04-27 12:42:53 +0500

    add up to 30 matters

--- app.js
+++ app.js
@@ -132,4 +132,146 @@
 
 // stringSomething();
 
+// 14)
+
+// function sonKvadrati(num2) {
+//   console.log(num2 ** 2);
+// }
+
+// sonKvadrati(3);
+
+// 15)
+
+// function noldan(raqam) {
+//   if (raqam <= 0) {
+//     console.log("True");
+//   } else {
+//     ("False");
+//   }
+// }
+
+// noldan(0);
+
+// 16)
+
+// function ichkiBurchaklar(n) {
+//   console.log((n - 2) * 180);
+// }
+
+// ichkiBurchaklar(7)
+
+// 17)
+
+// function ochkolar(x, y) {
+//   console.log(`Jami: ${x * 2 + y * 3}`);
+// }
+
+// ochkolar(3, 4);
+
+// 18)
+
+// let text = String(prompt("Text kiriting:"));
+
+// function stringEdabit(a) {
+//   a = "Edabit";
+//   console.log(text + a);
+// }
+
+// stringEdabit();
+
+// 19)
+
+// function ikkitaSon(x, y) {
+//   if (x + y < 100) {
+//     console.log("True");
+//   } else {
+//     console.log("False");
+//   }
+// }
+
+// ikkitaSon(66, 30);
+
+// 20)
+
+// function printArray(n) {
+//   for (i = 1; i <= n; i++) {
+//     console.log(`[${i}]`);
+//   }
+// }
+// printArray(7);
+
+// GALATI BOP QOLDI
+
+// 21)
+
+// function oyoqlar(tovuq, qoy, sigir) {
+//   console.log(tovuq * 2 + qoy * 4 + sigir * 4);
+// }
+
+// oyoqlar(2, 2, 2);
+
+// 23)
+
+// function tengMi(x, y) {
+//   if (x == y) {
+//     console.log("True");
+//   } else {
+//     console.log("False");
+//   }
+// }
+
+// tengMi(2, 2);
+
+// 24)
+
+// function futbolOchko(yutishlar, duranglar, maglubiyatlar) {
+//   console.log(`Jami: ${yutishlar * 3 + duranglar * 1}`); // MAGLUBIYAT NECHTA BOLGAN TAQDIRDAHAM 0 GA KOPAYGANDA 0 BOPKETADI!
+// }
+
+// futbolOchko(2, 3, 1);
+
+// 25)
+
+// function sekundlar(soat, minut) {
+//   console.log(soat * 3600 + minut * 60);
+// }
+
+// sekundlar(1, 5);
+
+// 26)
+// 27)
+
+// function tengMi(x, y) {
+//   if (x === y) {
+//     console.log("True");
+//   } else console.log("False");
+// }
+
+// tengMi(2, "2");
+
+// 28)
+
+// let bool = String(prompt("Boolean kiriting:"));
+
+// function booleanToString(bool) {
+//   console.log(`${bool}`);
+// }
+
+// booleanToString();
+
+// 29)
+
+// const arrow = (text) => {
+//   return text;
+// };
+// console.log(arrow(5));
+
+// 30)
+
+// function frameSoni(minut, frame) {
+//   console.log(minut * 60 * frame);
+// }
+
+// frameSoni(10, 25);
+
 // UYGA VAZIFA END
